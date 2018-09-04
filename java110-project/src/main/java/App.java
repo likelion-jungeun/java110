dfbbc91b155052e83ee867fa4a0fed0329721694
ko <dpebqkrtm11@naver.com>
2018-09-04 12:45:34 +0900

    반복문을 이용하여 입력 기능 추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -7,17 +7,23 @@ public class App {
         // 1) 키보드 입력을 처리할 객체 준비
         Scanner keyIn = new Scanner(System.in);
 
-        // 2) 사용자로부터 회원 정보 입력받기
-        System.out.print("이름 : ");
-        String name = keyIn.nextLine();// enter를 칠때까지 리턴하지 않음
+        while (true) {
+            // 2) 사용자로부터 회원 정보 입력받기
+            System.out.print("이름 : ");
+            String name = keyIn.nextLine();// enter를 칠때까지 리턴하지 않음
 
-        System.out.print("이메일 : ");
-        String email = keyIn.nextLine();
+            System.out.print("이메일 : ");
+            String email = keyIn.nextLine();
 
-        System.out.print("암호 : ");
-        String password = keyIn.nextLine();
+            System.out.print("암호 : ");
+            String password = keyIn.nextLine();
 
-        System.out.printf("%s, %s, %s \n", name, email, password);
+            System.out.printf("%s, %s, %s \n", name, email, password);
 
+            System.out.print("계속 등록하시겠습니까?(Y/n)");
+            String answer = keyIn.nextLine();
+            if (answer.toLowerCase().equals("n"))
+                break;
+        }
     }
 }
