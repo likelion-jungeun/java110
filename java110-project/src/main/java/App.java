bf7d657bf65b8a06e5a0e92973e07199304dce69
ko <dpebqkrtm11@naver.com>
2018-09-04 14:37:44 +0900

    배열을 이용하여 여러 회원의 정보를 저장

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -4,26 +4,39 @@ public class App {
 
     public static void main(String[] args) {
 
+        // 4) 저장할 배열을 만든다
+        String[] names = new String[100];
+        String[] emails = new String[100];
+        String[] passwords = new String[100];
+
+        int index = 0;
+
         // 1) 키보드 입력을 처리할 객체 준비
         Scanner keyIn = new Scanner(System.in);
 
+        // 3) 반복문을 만든다
         while (true) {
             // 2) 사용자로부터 회원 정보 입력받기
             System.out.print("이름 : ");
-            String name = keyIn.nextLine();// enter를 칠때까지 리턴하지 않음
+            names[index] = keyIn.nextLine();// enter를 칠때까지 리턴하지 않음
 
             System.out.print("이메일 : ");
-            String email = keyIn.nextLine();
+            emails[index] = keyIn.nextLine();
 
             System.out.print("암호 : ");
-            String password = keyIn.nextLine();
+            passwords[index] = keyIn.nextLine();
 
-            System.out.printf("%s, %s, %s \n", name, email, password);
+            index++;
 
             System.out.print("계속 등록하시겠습니까?(Y/n)");
             String answer = keyIn.nextLine();
             if (answer.toLowerCase().equals("n"))
                 break;
         }
+        // 5) 출력을만든다
+        for (int i = 0; i < index; i++) {
+            System.out.printf("%s, %s, %s\n", names[i], emails[i], passwords[i]);
+        }
+        keyIn.close();
     }
 }
