6b5df29e1ee1ed65f6149c2cbbca2e2eea206f09
ko <dpebqkrtm11@naver.com>
2018-09-04 15:24:20 +0900

    클래스 활용

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -2,40 +2,72 @@ import java.util.Scanner;
 
 public class App {
 
-    // 4) 저장할 배열을 만든다
-    static String[] names = new String[100];
-    static String[] emails = new String[100];
-    static String[] passwords = new String[100];
+    // 여러 속성의 값을 관리하기 쉽도록 사용자 정의 데이터 타입을 만들어 사용한다.
+    static class Member {
+        protected String name;
+        protected String email;
+        protected String password;
+
+        // 인스턴스의 메모리를 다루는 operator=setter/geeter==accessor==property==message
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+    }
+
+    static Member[] members = new Member[100];
 
     static int index = 0;
 
-    // 1) 키보드 입력을 처리할 객체 준비
     static Scanner keyIn = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        printMembers();
-        
         inputMembers();
-
+        printMembers();
         keyIn.close();
     }
 
-    // 6) 메소드들을 만들고 데이터들을 옮긴다
     static void printMembers() {
-        // 3) 반복문을 만든다
+        for (int i = 0; i < index; i++) {
+            System.out.printf("%s, %s, %s\n", members[i].getName(), members[i].getEmail(), members[i].getPassword());
+        }
+    }
+
+    static void inputMembers() {
+
         while (true) {
-            // 2) 사용자로부터 회원 정보 입력받기
+            Member m = new Member();
+
             System.out.print("이름 : ");
-            names[index] = keyIn.nextLine();// enter를 칠때까지 리턴하지 않음
+            m.setName(keyIn.nextLine());
 
             System.out.print("이메일 : ");
-            emails[index] = keyIn.nextLine();
+            m.setEmail(keyIn.nextLine());
 
             System.out.print("암호 : ");
-            passwords[index] = keyIn.nextLine();
-
-            index++;
+            m.setPassword(keyIn.nextLine());
+            members[index++] = m;
 
             System.out.print("계속 등록하시겠습니까?(Y/n)");
             String answer = keyIn.nextLine();
@@ -43,12 +75,5 @@ public class App {
                 break;
         }
 
-    }
-
-    // 5) 출력을만든다
-    static void inputMembers() {
-        for (int i = 0; i < index; i++) {
-            System.out.printf("%s, %s, %s\n", names[i], emails[i], passwords[i]);
-        }
     }
 }
