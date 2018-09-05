732a169f635fa745ed26958a96e26b90c6a90e87
ko <dpebqkrtm11@naver.com>
2018-09-05 12:02:47 +0900

    매니저 관리 기능 추가

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -2,157 +2,30 @@ import java.util.Scanner;
 
 public class App {
 
-    static class Member {
-        protected String name;
-        protected String email;
-        protected String password;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-    }
-
-    static class Student extends Member {
-
-        protected String school;
-        protected boolean working;
-        protected String tel;
-
-        public String getSchool() {
-            return school;
-        }
-
-        public void setSchool(String school) {
-            this.school = school;
-        }
-
-        public boolean isWorking() {
-            return working;
-        }
-
-        public void setWorking(boolean working) {
-            this.working = working;
-        }
-
-        public String getTel() {
-            return tel;
-        }
-
-        public void setTel(String tel) {
-            this.tel = tel;
-        }
-
-    }
-
-    static class Teacher extends Member {
-        protected String tel;
-        protected int pay;
-        protected String subjects;
-
-        public String getTel() {
-            return tel;
-        }
-
-        public void setTel(String tel) {
-            this.tel = tel;
-        }
-
-        public int getPay() {
-            return pay;
-        }
-
-        public void setPay(int pay) {
-            this.pay = pay;
-        }
-
-        public String getSubjects() {
-            return subjects;
-        }
-
-        public void setSubjects(String subjects) {
-            this.subjects = subjects;
-        }
-    }
-
-    static Student[] students = new Student[100];
-    static Teacher[] teachers = new Teacher[100];
-
-    static int studentIndex = 0; 
-    static int teacherIndex= 0; 
-
     static Scanner keyIn = new Scanner(System.in);
 
     public static void main(String[] args) {
+        StudentController.keyIn = keyIn;
+        TeacherController.keyIn = keyIn;
+        ManagerController.keyIn = keyIn;
 
         while (true) {
             String menu = promptMenu();
             if (menu.equals("1")) {
-                serviceStudentMenu();
-            }else if(menu.equals("2")) {
-                serviceTeacherMenu();
-            }
-            else if (menu.equals("0")) {
+                StudentController.serviceStudentMenu();
+            } else if (menu.equals("2")) {
+                TeacherController.serviceTeacherMenu();
+            } else if (menu.equals("3")) {
+                ManagerController.serviceManagerMenu();
+            } else if (menu.equals("0")) {
                 System.out.println("안녕히 가세요!");
                 break;
             }
         }
         keyIn.close();
+
     }
 
-    private static void serviceStudentMenu() {
-        while (true) {
-            System.out.println("학생관리> ");
-            String command = keyIn.nextLine();
-            if (command.equals("list")) {
-                printStudents();
-            } else if (command.equals("add")) {
-                inputStudents();
-            } else if (command.equals("quit")) {
-                break;
-            } else {
-                System.out.println("유효하지 않는 명령입니다.");
-            }
-        }
-    }
-
-    
-    private static void serviceTeacherMenu() {
-        while (true) {
-            System.out.println("강사관리> ");
-            String command = keyIn.nextLine();
-            if (command.equals("list")) {
-                printTeachers();
-            } else if (command.equals("add")) {
-                inputTeachers();
-            } else if (command.equals("quit")) {
-                break;
-            } else {
-                System.out.println("유효하지 않는 명령입니다.");
-            }
-        }
-    }
-    
     private static String promptMenu() {
         System.out.println("[메뉴]");
         System.out.println("1.학생 관리");
@@ -174,101 +47,4 @@ public class App {
             }
         }
     }
-
-    static void printStudents() {
-        int count = 0;
-        for (Student s : students) {
-
-            if (count++ == studentIndex) // 이거 후위연산자야!!
-                break;
-            // students 자리에는배열 또는 컬렉션이 올 수 있음
-            System.out.printf("%s, %s, %s, %s, %b, %s \n", s.getName(), s.getEmail(), s.getPassword(), s.getSchool(),
-                    s.isWorking(), s.getTel());
-
-        }
-    }
-    
-    static void printTeachers() {
-        int count = 0;
-        for (Teacher s : teachers) {
-
-            if (count++ == teacherIndex)
-                break;
-            System.out.printf("%s, %s, %s, %s, %d, [%s] \n", 
-                                s.getName(), 
-                                s.getEmail(), 
-                                s.getPassword(), 
-                                s.getTel(),
-                                s.getPay(), 
-                                s.getSubjects());
-        }
-    }
-
-    static void inputStudents() {
-
-        while (true) {
-            Student m = new Student();
-
-            System.out.print("이름 : ");
-            m.setName(keyIn.nextLine());
-
-            System.out.print("이메일 : ");
-            m.setEmail(keyIn.nextLine());
-
-            System.out.print("암호 : ");
-            m.setPassword(keyIn.nextLine());
-
-            System.out.print("최종학력 : ");
-            m.setSchool(keyIn.nextLine());
-
-            System.out.print("재직여부 :(true/false) ");
-            m.setWorking(Boolean.parseBoolean(keyIn.nextLine()));
-
-            System.out.print("전화번호 : ");
-            m.setTel(keyIn.nextLine());
-
-            students[studentIndex++] = m;
-
-            System.out.print("계속 등록하시겠습니까?(Y/n)");
-            String answer = keyIn.nextLine();
-            if (answer.toLowerCase().equals("n"))
-                break;
-        }
-
-    }
-    
-    
-    static void inputTeachers() {
-
-        while (true) {
-            Teacher m = new Teacher();
-
-            System.out.print("이름 : ");
-            m.setName(keyIn.nextLine());
-
-            System.out.print("이메일 : ");
-            m.setEmail(keyIn.nextLine());
-
-            System.out.print("암호 : ");
-            m.setPassword(keyIn.nextLine());
-
-            System.out.print("전화 : ");
-            m.setTel(keyIn.nextLine());
-
-            System.out.print("급여 : ");
-            m.setPay(Integer.parseInt(keyIn.nextLine()));
-            
-            System.out.print("강의과목(예: 자바,C,C++) : ");
-            m.setSubjects(keyIn.nextLine());
-
-            teachers[teacherIndex++] = m;
-
-            System.out.print("계속 등록하시겠습니까?(Y/n)");
-            String answer = keyIn.nextLine();
-            if (answer.toLowerCase().equals("n"))
-                break;
-        }
-
-    }
-    
 }
---
+++ java110-project/src/main/java/StudentController.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class StudentController {
+
+    static Scanner keyIn;
+
+    static class Student extends Member {
+
+        protected String school;
+        protected boolean working;
+        protected String tel;
+
+        public String getSchool() {
+            return school;
+        }
+
+        public void setSchool(String school) {
+            this.school = school;
+        }
+
+        public boolean isWorking() {
+            return working;
+        }
+
+        public void setWorking(boolean working) {
+            this.working = working;
+        }
+
+        public String getTel() {
+            return tel;
+        }
+
+        public void setTel(String tel) {
+            this.tel = tel;
+        }
+
+    }
+
+    static Student[] students = new Student[100];
+    static int studentIndex = 0;
+
+    static void serviceStudentMenu() {
+        while (true) {
+            System.out.println("학생관리> ");
+            String command = keyIn.nextLine();
+            if (command.equals("list")) {
+                printStudents();
+            } else if (command.equals("add")) {
+                inputStudents();
+            } else if (command.equals("quit")) {
+                break;
+            } else {
+                System.out.println("유효하지 않는 명령입니다.");
+            }
+        }
+    }
+
+    static void printStudents() {
+        int count = 0;
+        for (Student s : students) {
+
+            if (count++ == studentIndex) 
+                break;
+            System.out.printf("%s, %s, %s, %s, %b, %s \n", s.getName(), s.getEmail(), s.getPassword(), s.getSchool(),
+                    s.isWorking(), s.getTel());
+
+        }
+    }
+
+    static void inputStudents() {
+
+        while (true) {
+            Student m = new Student();
+
+            System.out.print("이름 : ");
+            m.setName(keyIn.nextLine());
+
+            System.out.print("이메일 : ");
+            m.setEmail(keyIn.nextLine());
+
+            System.out.print("암호 : ");
+            m.setPassword(keyIn.nextLine());
+
+            System.out.print("최종학력 : ");
+            m.setSchool(keyIn.nextLine());
+
+            System.out.print("재직여부 :(true/false) ");
+            m.setWorking(Boolean.parseBoolean(keyIn.nextLine()));
+
+            System.out.print("전화번호 : ");
+            m.setTel(keyIn.nextLine());
+
+            students[studentIndex++] = m;
+
+            System.out.print("계속 등록하시겠습니까?(Y/n)");
+            String answer = keyIn.nextLine();
+            if (answer.toLowerCase().equals("n"))
+                break;
+        }
+
+    }
+
+}
