436ff8e72d8855b812c6217f95016706fe86e0e0
ko <dpebqkrtm11@naver.com>
2018-09-18 11:32:37 +0900

    ServerApp에 HTTP 프로토콜 적용, 웹 브라우저 사용

--- java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
+++ java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
@@ -1,8 +1,10 @@
 package bitcamp.java110.cms.server;
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,7 +18,7 @@ public class ServerApp {
     ClassPathXmlApplicationContext iocContainer;
     RequestMappingHandlerMapping requestHandlerMap;
 
-    public ServerApp() throws Exception{
+    public ServerApp() throws Exception {
         createIoCContainer();
         logBeansOfContainer();
         processRequestMappingAnnotation();
@@ -28,6 +30,7 @@ public class ServerApp {
     }
 
     private void processRequestMappingAnnotation() {
+
         requestHandlerMap = new RequestMappingHandlerMapping();
         String[] names = iocContainer.getBeanDefinitionNames();
 
@@ -38,95 +41,109 @@ public class ServerApp {
     }
 
     private void logBeansOfContainer() {
+
         System.out.println("--------------------------");
         String[] nameList = iocContainer.getBeanDefinitionNames();
 
-        for(String name:nameList) {
+        for (String name : nameList) {
             System.out.println(name);
         }
         System.out.println("--------------------------");
     }
 
-    public void service() throws Exception{
+    public void service() throws Exception {
 
         // 클라이언트 연결을 기다리는 서버 소켓 준비
         ServerSocket serverSocket = new ServerSocket(8888);
+
         System.out.println("서버 실행 중...");
 
-        while(true) {
+        while (true) {
             Socket socket = serverSocket.accept();
             System.out.println("클라이언트가 연결되었음!");
             RequestWorker worker = new RequestWorker(socket);
-            /*Thread t = new Thread(worker);
-            t.start();*/
-            new Thread(worker).start(); // 위두줄과같은코드
+            new Thread(worker).start(); 
         }
     }
-
 
     public static void main(String[] args) throws Exception {
         ServerApp serverApp = new ServerApp();
         serverApp.service();
     }
 
-
-    class RequestWorker implements Runnable{
+    class RequestWorker implements Runnable {
         Socket socket;
 
         public RequestWorker(Socket socket) {
             this.socket = socket;
+
         }
 
         @Override
         public void run() {
-
             // 이 메서드에 "main" 스레드에서 분리하여 독립적으로 수행할 코드를 둔다.
-
-            try(
-                    Socket socket = this.socket; // 이렇게 로컬변수로 따로만든이유는 try()에 값이 있어야지만 try를 벗어날때 자동으로 close를 호출하기때문임. 
+            try (
+                    Socket socket = this.socket; // 이렇게 로컬변수로 따로만든이유는 try()에 값이 있어야지만 try를 벗어날때 자동으로 close를 호출하기때문임.
                     PrintWriter out = new PrintWriter(
                             new BufferedOutputStream(
                                     socket.getOutputStream()));
                     BufferedReader in = new BufferedReader(
                             new InputStreamReader(
-                                    socket.getInputStream())); 
-                    ){
+                                    socket.getInputStream()));
+                    ) {
 
-                String requestLine = in.readLine();
                 System.out.println("클라이언트 요청 받았음!");
+                boolean firstLine = true;
+                String requestURI = "";
+                while (true) {
+                    String line = in.readLine();
+                    System.out.println(line);
 
+                    if (line.length() == 0)
+                        break;
+
+                    if (firstLine) {
+                        requestURI = line.split(" ")[1];
+                        firstLine = false;
+                    }
+                }
                 // 요청 객체 준비
-                Request request = new Request(requestLine);
+                // => requestURI에서 첫 번째 문자인 '/'는 제거한다.
+                Request request = new Request(requestURI.substring(1));
 
                 // 응답 객체 준비
-                Response response = new Response(out);
-
-
-                RequestMappingHandler mapping = 
+                StringWriter strWriter = new StringWriter();
+                PrintWriter bufOut = new PrintWriter(strWriter);
+                Response response = new Response(bufOut);
+                RequestMappingHandler mapping =
                         requestHandlerMap.getMapping(request.getAppPath());
 
                 if (mapping == null) {
-                    out.println("해당 요청을 처리할 수 없습니다.");
-                    out.println();
-                    out.flush();
+                    bufOut.println("해당 요청을 처리할 수 없습니다.");
                     return;
                 }
 
                 try {
                     // 요청 핸들러 호출
-                    mapping.getMethod().invoke(mapping.getInstance(), request,response); // static메소드를 호출할땐 첫번재매개변수에 null을주면됨
+                    mapping.getMethod().invoke(mapping.getInstance(), request, response); // static메소드를 호출할땐 첫번재매개변수에
                 } catch (Exception e) {
                     e.printStackTrace();
-                    out.println("요청 처리 중에 오류가 발생했습니다.");
+                    bufOut.println("요청 처리 중에 오류가 발생했습니다.");
                 }
-                out.println();
-                out.flush();
-            }catch(Exception e) {
+                responseHTTPMessage(out, strWriter.toString());
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
-            }finally {
+            } finally {
                 System.out.println("클라이언트에게 응답했음!");
                 System.out.println("클라이언트와 연결을 끊음!");
             }
         }// run()
+        private void responseHTTPMessage(PrintWriter out, String message) {
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-type: text/plain;charset=UTF-8"); // client에게 보내는 텍스트는 순수한 텍스트다라는걸 알려주는부분.
+            out.println();
+            out.print(message);
+            out.flush();
+        }
     }// RequestWorker class
 }// ServletApp class
