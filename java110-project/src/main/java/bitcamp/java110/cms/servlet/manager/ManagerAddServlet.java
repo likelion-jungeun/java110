8f4607c863c688f85f24f6ebbb531009586d1bf9
ko <dpebqkrtm11@naver.com>
2018-09-28 15:17:13 +0900

    리프래시, 리다이랙트 적용하기

--- java110-project/src/main/java/bitcamp/java110/cms/servlet/manager/ManagerAddServlet.java
+++ java110-project/src/main/java/bitcamp/java110/cms/servlet/manager/ManagerAddServlet.java
@@ -15,11 +15,13 @@ public class ManagerAddServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) // doget,dopost 둘다하고싶다면 service를하면됨
+    protected void doPost(
+            HttpServletRequest request,
+            HttpServletResponse response) // doget,dopost 둘다하고싶다면 service를하면됨
             throws ServletException, IOException {
 
         request.setCharacterEncoding("UTF-8");
-        
+
         Manager m = new Manager();
         m.setName(request.getParameter("name"));
         m.setEmail(request.getParameter("email"));
@@ -27,30 +29,36 @@ public class ManagerAddServlet extends HttpServlet {
         m.setTel(request.getParameter("tel"));
         m.setPosition(request.getParameter("position"));
 
-        response.setContentType("text/html;charset=UTF-8");
-
-        PrintWriter out = response.getWriter();
-
-        ManagerDao managerDao = (ManagerDao) 
-                this.getServletContext().getAttribute("managerDao");
-
-        out.println("<!DOCThtml>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset='UTF-8'>");
-        out.println("<title>매니저 관리</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>매니저 등록 결과</h1>");
+        ManagerDao managerDao = (ManagerDao) this.getServletContext().getAttribute("managerDao");
 
         try {
             managerDao.insert(m);
-            out.println("<p>저장하였습니다.</p>");
+            // 오류 없이 등록에 성공했으면, 목록 페이지를 다시 요청하라고
+            // redirect 명령을 보낸다.
+            response.sendRedirect("list");
+            
         } catch (Exception e) {
             e.printStackTrace();
-            out.println("<p>등록 중 오류 발생!</p>");
+            // 등록 오류 결과를 출력하고 1초가 경과한 후에 목록 페이지를 요청하도록 "리프래시" 명령을 설정한다
+            // => 응답할 때 응답 헤더로 리프래시에 대한 명령을 웹브라우저에게 전달한다.
+            response.setHeader("Refresh", "3;url=list");
+            
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            
+            out.println("<!DOCThtml>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<meta charset='UTF-8'>");
+            out.println("<title>매니저 관리</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>매니저 등록 오류!</h1>");
+            out.printf("<p>%s</p>\n", e.getMessage());
+            
+            out.println("<p>잠시 기다리면 자동으로 목록페이지로 이동합니다</p>");
+            out.println("</body>");
+            out.println("</html>");
         }
-        out.println("</body>");
-        out.println("</html>");
     }
 }
--- java110-project/src/main/java/bitcamp/java110/cms/servlet/teacher/TeacherAddServlet.java
+++ java110-project/src/main/java/bitcamp/java110/cms/servlet/teacher/TeacherAddServlet.java
@@ -33,30 +33,32 @@ public class TeacherAddServlet extends HttpServlet {
         t.setPay(Integer.parseInt(request.getParameter("pay")));
         t.setSubjects(request.getParameter("subject"));
 
-        response.setContentType("text/html;charset=UTF-8");
-        
-        PrintWriter out =response.getWriter();
-        TeacherDao teacherDao = (TeacherDao)this.getServletContext()
+       TeacherDao teacherDao = (TeacherDao)this.getServletContext()
                 .getAttribute("teacherDao");
         
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset='UTF-8'>");
-        out.println("<title>강사 관리</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>강사 등록 결과</h1>");
-
         try {
             teacherDao.insert(t);
-            out.println("<p>저장하였습니다.</p>");
+            response.sendRedirect("list");
+            
         } catch (Exception e) {
             e.printStackTrace();
-            out.println("<p>등록 중 오류 발생!</p>");
+            response.setHeader("Refresh", "1;url=list");
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out =response.getWriter();
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<meta charset='UTF-8'>");
+            out.println("<title>강사 관리</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>강사 등록 오류!</h1>");
+            out.printf("<p>%s</p>\n", e.getMessage());
+            out.println("<p>잠시 기다리면 자동으로 목록페이지로 이동합니다</p>");
+            out.println("</body>");
+            out.println("</html>");
         }
-        out.println("</body>");
-        out.println("</html>");
         
         
     }
