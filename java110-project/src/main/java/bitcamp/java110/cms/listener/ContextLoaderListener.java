5bb35cc370c5e08562ea173339ba0c8396cc93c9
ko <dpebqkrtm11@naver.com>
2018-10-19 16:27:39 +0900

    .

--- java110-project/src/main/java/bitcamp/java110/cms/listener/ContextLoaderListener.java
+++ java110-project/src/main/java/bitcamp/java110/cms/listener/ContextLoaderListener.java
@@ -1,21 +1,14 @@
 package bitcamp.java110.cms.listener;
 
-import java.io.InputStream;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
- 
-import bitcamp.java110.cms.service.impl.AuthServiceImpl;
-import bitcamp.java110.cms.service.impl.ManagerServiceImpl;
-import bitcamp.java110.cms.service.impl.StudentServiceImpl;
-import bitcamp.java110.cms.service.impl.TeacherServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-//@WebListener
+import bitcamp.java110.cms.AppConfig;
+
 public class ContextLoaderListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
@@ -24,33 +17,20 @@ public class ContextLoaderListener implements ServletContextListener {
         ServletContext sc = sce.getServletContext();
 
         try {
-            String resource = "bitcamp/java110/cms/conf/mybatis-config.xml";
-            InputStream inputStream = Resources.getResourceAsStream(resource);
-            SqlSessionFactory sqlSessionFactory =
-              new SqlSessionFactoryBuilder().build(inputStream);
+            ApplicationContext context = 
+                    new AnnotationConfigApplicationContext(AppConfig.class);
             
-            
-            // 서비스 객체 준비하기
-            ManagerServiceImpl managerService = new ManagerServiceImpl();
-            managerService.setSqlSessionFactory(sqlSessionFactory);
-            
-            StudentServiceImpl studentService = new StudentServiceImpl();
-            studentService.setSqlSessionFactory(sqlSessionFactory);
-            
-            TeacherServiceImpl teacherService = new TeacherServiceImpl();
-            teacherService.setSqlSessionFactory(sqlSessionFactory);
-            
-            AuthServiceImpl authService = new AuthServiceImpl();
-            authService.setSqlSessionFactory(sqlSessionFactory);
-            
-            // 서블릿에서 Service를 이용할 수 있도록 ServletContext 보관소에 저장하기
-            sc.setAttribute("managerService", managerService);
-            sc.setAttribute("studentService", studentService);
-            sc.setAttribute("teacherService", teacherService);
-            sc.setAttribute("authService", authService);
+            sc.setAttribute("iocContainer", context);
             
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
+
+
+
+
+
+
