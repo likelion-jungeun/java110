c9843cf6e711f84a452c07247f72470112664409
ko <dpebqkrtm11@naver.com>
2018-10-19 12:27:25 +0900

    Sping IoC 컨테이너 적용

--- java110-project/src/main/java/bitcamp/java110/cms/listener/ContextLoaderListener.java
+++ java110-project/src/main/java/bitcamp/java110/cms/listener/ContextLoaderListener.java
@@ -1,26 +1,14 @@
 package bitcamp.java110.cms.listener;
 
-import java.io.InputStream;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import bitcamp.java110.cms.dao.impl.ManagerMysqlDao;
-import bitcamp.java110.cms.dao.impl.MemberMysqlDao;
-import bitcamp.java110.cms.dao.impl.PhotoMysqlDao;
-import bitcamp.java110.cms.dao.impl.StudentMysqlDao;
-import bitcamp.java110.cms.dao.impl.TeacherMysqlDao;
-import bitcamp.java110.cms.service.impl.AuthServiceImpl;
-import bitcamp.java110.cms.service.impl.ManagerServiceImpl;
-import bitcamp.java110.cms.service.impl.StudentServiceImpl;
-import bitcamp.java110.cms.service.impl.TeacherServiceImpl;
+import bitcamp.java110.cms.AppConfig;
 
-//@WebListener
 public class ContextLoaderListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
@@ -29,53 +17,10 @@ public class ContextLoaderListener implements ServletContextListener {
         ServletContext sc = sce.getServletContext();
 
         try {
-            String resource = "bitcamp/java110/cms/conf/mybatis-config.xml";
-            InputStream inputStream = Resources.getResourceAsStream(resource);
-            SqlSessionFactory sqlSessionFactory =
-              new SqlSessionFactoryBuilder().build(inputStream);
+            ApplicationContext context = 
+                    new AnnotationConfigApplicationContext(AppConfig.class);
             
-            // DAO 객체 생성 및 DB 커네션풀 주입하기
-            MemberMysqlDao memberDao = new MemberMysqlDao();
-            memberDao.setSqlSessionFactory(sqlSessionFactory);
-            
-            PhotoMysqlDao photoDao = new PhotoMysqlDao();
-            photoDao.setSqlSessionFactory(sqlSessionFactory);
-
-            ManagerMysqlDao managerDao = new ManagerMysqlDao();
-            managerDao.setSqlSessionFactory(sqlSessionFactory);
-            
-            StudentMysqlDao studentDao = new StudentMysqlDao();
-            studentDao.setSqlSessionFactory(sqlSessionFactory);
-            
-            TeacherMysqlDao teacherDao = new TeacherMysqlDao();
-            teacherDao.setSqlSessionFactory(sqlSessionFactory);
-            
-            // 서비스 객체 준비하기
-            ManagerServiceImpl managerService = new ManagerServiceImpl();
-            managerService.setMemberDao(memberDao);
-            managerService.setManagerDao(managerDao);
-            managerService.setPhotoDao(photoDao);
-            
-            StudentServiceImpl studentService = new StudentServiceImpl();
-            studentService.setMemberDao(memberDao);
-            studentService.setStudentDao(studentDao);
-            studentService.setPhotoDao(photoDao);
-            
-            TeacherServiceImpl teacherService = new TeacherServiceImpl();
-            teacherService.setMemberDao(memberDao);
-            teacherService.setTeacherDao(teacherDao);
-            teacherService.setPhotoDao(photoDao);
-            
-            AuthServiceImpl authService = new AuthServiceImpl();
-            authService.setManagerDao(managerDao);
-            authService.setStudentDao(studentDao);
-            authService.setTeacherDao(teacherDao);
-            
-            // 서블릿에서 Service를 이용할 수 있도록 ServletContext 보관소에 저장하기
-            sc.setAttribute("managerService", managerService);
-            sc.setAttribute("studentService", studentService);
-            sc.setAttribute("teacherService", teacherService);
-            sc.setAttribute("authService", authService);
+            sc.setAttribute("iocContainer", context);
             
         } catch (Exception e) {
             e.printStackTrace();
--- java110-project/src/main/java/bitcamp/java110/cms/service/impl/AuthServiceImpl.java
+++ java110-project/src/main/java/bitcamp/java110/cms/service/impl/AuthServiceImpl.java
@@ -2,49 +2,49 @@ package bitcamp.java110.cms.service.impl;
 
 import java.util.HashMap;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import bitcamp.java110.cms.dao.ManagerDao;
 import bitcamp.java110.cms.dao.StudentDao;
 import bitcamp.java110.cms.dao.TeacherDao;
 import bitcamp.java110.cms.domain.Member;
 import bitcamp.java110.cms.service.AuthService;
 
+@Service
 public class AuthServiceImpl implements AuthService {
 
-    ManagerDao managerDao;
-    StudentDao studentDao;
-    TeacherDao teacherDao;
+    @Autowired SqlSessionFactory sqlSessionFactory;
 
-    public void setManagerDao(ManagerDao managerDao) {
-        this.managerDao = managerDao;
-    }
-
-    public void setStudentDao(StudentDao studentDao) {
-        this.studentDao = studentDao;
-    }
-
-    public void setTeacherDao(TeacherDao teacherDao) {
-        this.teacherDao = teacherDao;
-    }
-    
     @Override
     public Member getMember(
             String email, String password, String memberType) {
         
-        HashMap<String,Object> params = new HashMap<>();
-        params.put("email", email);
-        params.put("password", password);
-        
-        if (memberType.equals("manager")) {
-            return managerDao.findByEmailPassword(params);
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            HashMap<String,Object> params = new HashMap<>();
+            params.put("email", email);
+            params.put("password", password);
             
-        } else if (memberType.equals("student")) {
-            return studentDao.findByEmailPassword(params);
-            
-        } else if (memberType.equals("teacher")) {
-            return teacherDao.findByEmailPassword(params);
-            
-        } else {
-            return null;
+            if (memberType.equals("manager")) {
+                ManagerDao managerDao = 
+                        session.getMapper(ManagerDao.class);
+                return managerDao.findByEmailPassword(params);
+                
+            } else if (memberType.equals("student")) {
+                StudentDao studentDao = 
+                        session.getMapper(StudentDao.class);
+                return studentDao.findByEmailPassword(params);
+                
+            } else if (memberType.equals("teacher")) {
+                TeacherDao teacherDao = 
+                        session.getMapper(TeacherDao.class);
+                return teacherDao.findByEmailPassword(params);
+                
+            } else {
+                return null;
+            }
         }
     }
     
